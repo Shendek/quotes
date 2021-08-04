1a4f91bcbfa6ebea65fb1035d6ee3475da5c27ad
Shendek <32926981+Shendek@users.noreply.github.com>
2021-08-04 08:19:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('./sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 document.addEventListener('click',function(){
 	const el = event.target;
 	if(el.classList.contains("change_color")){
